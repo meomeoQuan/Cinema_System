b28b33b858e21e556df48dd3e33bad4624b1a39e
HuyTP-356 <huytpde180356@fpt.edu.vn>
2025-03-30 15:53:01 +0700

    Add schedule form

--- Cinema_System/Areas/Admin/Controllers/SchedulesController.cs
+++ Cinema_System/Areas/Admin/Controllers/SchedulesController.cs
@@ -17,28 +17,36 @@ namespace Cinema_System.Areas.Admin.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var schedules = await _unitOfWork.showTime.GetAllAsync("Movie,Cinema");
+            var schedules = await _unitOfWork.showTime.GetAllAsync("Movie,Room.Cinema");
+            var movies = await _unitOfWork.Movie.GetAllAsync();
+            var cinemas = await _unitOfWork.Cinema.GetAllAsync();
+            //var
+            //var Showtime
+            ViewBag.Movies = movies.Select(m => new { Id = m.MovieID, Title = m.Title }).ToList();
+            ViewBag.Cinemas = cinemas.Select(c => new { Id = c.CinemaID, Name = c.Name }).ToList();
             return View(schedules);
         }
 
-        [HttpGet("GetAll")]
-        public async Task<IActionResult> GetAll()
-        {
-            // Sử dụng includeProperties để load dữ liệu từ các bảng liên quan
-            var schedules = await _unitOfWork.showTime.GetAllAsync(includeProperties: "Movie,Cinema");
+        //[HttpGet("GetAll")]
+        //public async Task<IActionResult> GetAll()
+        //{
+        //    // Sử dụng includeProperties để load dữ liệu từ các bảng liên quan
+        //    var schedules = await _unitOfWork.showTime.GetAllAsync(includeProperties: "Movie,Cinema");
 
-            // Kiểm tra xem dữ liệu đã được load đúng cách chưa
-            var schedulesList = schedules.Select(s => new
-            {
-                MovieName = s.Movie?.Title ?? "Unknown", // Kiểm tra null
-                CinemaName = s.Cinema?.Name ?? "Unknown", // Kiểm tra null
-                s.RoomID,
-                s.ShowDates,
-                s.ShowTimes,
-                s.AvailableTicketQuantity
-            }).ToList();
+        //    // Kiểm tra xem dữ liệu đã được load đúng cách chưa
+        //    var schedulesList = schedules.Select(s => new
+        //    {
+        //        MovieName = s.Movie?.Title ?? "Unknown", // Kiểm tra null
+        //        CinemaName = s.Cinema?.Name ?? "Unknown", // Kiểm tra null
+        //        s.RoomID,
+        //        s.ShowDates,
+        //        s.ShowTimes,
+        //        s.AvailableTicketQuantity
+        //    }).ToList();
 
-            return Json(new { data = schedulesList });
-        }
+        //    return Json(new { data = schedulesList });
+        //}
+
+
     }
 }
